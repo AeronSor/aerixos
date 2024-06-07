099d5fd3dfba9f4ac2d35aa33686188f6e371339
AeronSor <aeronsor@gmail.com>
2024-06-07 20:30:11 -0300

    68 current  2024-06-07 20:30:05  24.05.20240427.58a1abd   6.6.28                          on-the-go

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -6,7 +6,7 @@ ranger
 alejandra . &>/dev/null	# Use a nix code formatter
 git diff -U0 *.nix
 echo "NixOS Rebuilding..."
-sudo nixos-rebuild --flake ~/Repos/aerixos/#aeron &>nixos-switch.log || (cat nixos-switch.log | grep --collor error && false)
+sudo nixos-rebuild switch --flake ~/Repos/aerixos/#aeron &>nixos-switch.log || (cat nixos-switch.log | grep --collor error && false)
 gen=$(nixos-rebuild list-generations | grep current)
 git commit -am "$gen"
 popd	# Return to said directory
